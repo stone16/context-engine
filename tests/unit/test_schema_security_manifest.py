e7f0892577eca608cac80ed143c248285893b3da
stone <stone2paul@gmail.com>
2026-07-30 23:50:51 +0800

    test: align membership grant inventory

--- tests/unit/test_schema_security_manifest.py
+++ tests/unit/test_schema_security_manifest.py
@@ -1558,6 +1558,7 @@ def test_membership_manifest_requires_exact_user_actor_and_read_only_runtime() -
         "context_engine_action_prepare_definer": ["SELECT"],
         "context_engine_action_execute_definer": ["SELECT"],
         "context_engine_citation_definer": ["SELECT"],
+        "context_engine_access_policy_definer": ["SELECT"],
         "context_engine_file_dispatch_definer": [
             "SELECT",
             "UPDATE status, valid_from, valid_until",
